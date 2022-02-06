7e7e3b6b40d2448add6b905d50a2e1a96c00394c
MohamedGEDI <gedimohammed@gmail.com>
2022-02-06 14:05:30 +0000

    fix

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -74,7 +74,7 @@ class DBStorage:
     def close(self):
         """call remove() method on the private session attribute"""
         self.__session.remove()
-           
+
     def get(self, cls, id):
         '''retrieves an object of type cls with the passed id
         or none if not found'''
